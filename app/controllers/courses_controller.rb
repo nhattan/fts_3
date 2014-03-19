5e0f1c6c219baf77fe9c1d00f976039758a6fdd6
Nhat Tan <nhattan2012@gmail.com>
2014-03-19 13:51:46 +0700

    Finish view user list

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,12 +2,9 @@ class CoursesController < ApplicationController
   before_action :signed_in_user
   before_action :trainee_user
 
-  def index
-    @user_courses = current_user.user_courses
-  end
-
   def show
     @course = Course.find params[:id]
-    @user_course = current_user.user_courses.find_by params[:id]
+    @user_course = current_user.user_courses.find_by id: params[:id]
+    redirect_to root_url if @user_course.nil?
   end
 end
